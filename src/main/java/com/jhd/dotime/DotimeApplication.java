ca7d7574cec58f7467ffe7a7dfaab9f540631399
hoonbro <wlgns1588@naver.com>
2023-01-15 17:53:22 +0900

    init

---
+++ src/main/java/com/jhd/dotime/DotimeApplication.java
@@ -0,0 +1,13 @@
+package com.jhd.dotime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhd/dotime/DotimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhd.dotime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
