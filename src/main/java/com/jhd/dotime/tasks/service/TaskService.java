03f257262214016b5d9074334392e191975be68a
LEEHYUNDONG <hd.lee1@cj.net>
2023-01-16 13:23:12 +0900

    feat :[20230116][이현동] Service, Repo Test code 작성 - Test 코드를 기반으로 기본 로직 작성 - Task객체와 crud 기능을 포함한 service 코드 작성 - Repo crud 작성 - 우선 Appconfig를 사용하서 bean에 등록했음

--- src/main/java/com/jhd/dotime/tasks/service/TaskService.java
+++ src/main/java/com/jhd/dotime/tasks/service/TaskService.java
@@ -1,4 +1,15 @@
 package com.jhd.dotime.tasks.service;
 
+import com.jhd.dotime.tasks.entity.Task;
+
+import java.time.LocalDateTime;
+
 public interface TaskService {
+    void insert(Task task);
+
+    Task findTask(Long id);
+
+    void delete(Long id);
+
+    void update(Long id, String title_edited, String content_edited, LocalDateTime updated_time);
 }
