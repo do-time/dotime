d607565dafb5fdb9420f72185d7a1da292c55fb6
hoonbro <wlgns1588@naver.com>
2023-01-30 22:50:39 +0900

    update: [20230118][변지훈] member entity 기초 작성 - Role을 제외한 Member entity 작성

--- src/main/java/com/jhd/dotime/members/entity/Member.java
+++ src/main/java/com/jhd/dotime/members/entity/Member.java
@@ -1,4 +1,29 @@
 package com.jhd.dotime.members.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+//@NoArgsConstructor
+@Entity
 public class Member {
+    @Id
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    private String profileImage;
+
+    private Date createdDate;
+
+    private Date updatedDate;
 }
