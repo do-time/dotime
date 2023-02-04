a1b3d2e942fcdd27b6284f00693820a86cf2be52
hoonbro <wlgns1588@naver.com>
2023-02-04 20:29:00 +0900

    update: [20230204][변지훈] CRUD + password 변경 메서드 추가 - CRUD + password 변경 메서드 추가 - Optional 미사용

--- src/main/java/com/jhd/dotime/members/service/MemberService.java
+++ src/main/java/com/jhd/dotime/members/service/MemberService.java
@@ -1,4 +1,16 @@
 package com.jhd.dotime.members.service;
 
+import com.jhd.dotime.members.dto.MemberDto;
+import com.jhd.dotime.members.entity.Member;
+
 public interface MemberService {
+    void createMember(MemberDto memberDto);
+
+    Member getMember(String email);
+
+    void updateMember(MemberDto memberDto);
+
+    void updatePassword(String email, String password);
+
+    void deleteMember(String email);
 }
--- src/main/java/com/jhd/dotime/members/service/MemberServiceImpl.java
+++ src/main/java/com/jhd/dotime/members/service/MemberServiceImpl.java
@@ -1,4 +1,39 @@
 package com.jhd.dotime.members.service;
 
+import com.jhd.dotime.members.dto.MemberDto;
+import com.jhd.dotime.members.entity.Member;
+import com.jhd.dotime.members.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void createMember(MemberDto memberDto) {
+        memberRepository.createMember(memberDto);
+    }
+
+    @Override
+    public Member getMember(String email) {
+        return memberRepository.getMember(email);
+    }
+
+    @Override
+    public void updateMember(MemberDto memberDto) {
+        memberRepository.updateMember(memberDto);
+    }
+
+    @Override
+    public void updatePassword(String email, String password) {
+        memberRepository.updatePassword(email, password);
+    }
+
+    @Override
+    public void deleteMember(String email) {
+        memberRepository.deleteMember(email);
+    }
 }
