a1886faa6c9521e29ccd8960219a7c0c9c57f5f3
hoonbro <wlgns1588@naver.com>
2023-02-04 20:27:13 +0900

    update: [20230204][변지훈] CRUD + password 변경 메서드 추가 - CRUD + password 변경 메서드 추가

--- src/main/java/com/jhd/dotime/members/repository/MemberRepository.java
+++ src/main/java/com/jhd/dotime/members/repository/MemberRepository.java
@@ -1,4 +1,17 @@
 package com.jhd.dotime.members.repository;
 
+import com.jhd.dotime.members.dto.MemberDto;
+import com.jhd.dotime.members.entity.Member;
+
 public interface MemberRepository {
+
+    void createMember(MemberDto memberDto);
+
+    Member getMember(String email);
+
+    void updateMember(MemberDto memberDto);
+
+    void updatePassword(String email, String password);
+
+    void deleteMember(String email);
 }
