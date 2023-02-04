e268637bbb373815afa171db9af4e2f0817bed61
hoonbro <wlgns1588@naver.com>
2023-02-04 20:28:15 +0900

    update: [20230204][변지훈] CRUD + password 변경 메서드 추가 - CRUD + password 변경 메서드 추가 - DB 연결전 hashMap 사용

--- src/main/java/com/jhd/dotime/members/repository/MemberRepositoryImpl.java
+++ src/main/java/com/jhd/dotime/members/repository/MemberRepositoryImpl.java
@@ -1,7 +1,58 @@
 package com.jhd.dotime.members.repository;
 
+import com.jhd.dotime.members.dto.MemberDto;
+import com.jhd.dotime.members.entity.Member;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
 
 @Repository
 public class MemberRepositoryImpl implements MemberRepository{
+
+    private final static HashMap<String, Member> DB = new HashMap<>();
+
+
+    @Override
+    public void createMember(MemberDto memberDto) {
+        Member member = new Member();
+
+        member.setId(DB.size()+1L);
+        member.setEmail(memberDto.getEmail());
+        member.setUsername(memberDto.getUsername());
+        member.setPassword(memberDto.getPassword());
+        member.setProfileImage(memberDto.getProfileImage());
+        member.setCreatedDate(LocalDateTime.now());
+        member.setUpdatedDate(LocalDateTime.now());
+    }
+
+    @Override
+    public Member getMember(String email) {
+        return DB.get(email);
+    }
+
+    @Override
+    public void updateMember(MemberDto memberDto) {
+        Member member = DB.get(memberDto.getEmail());
+
+        member.setUsername(memberDto.getUsername());
+        member.setUpdatedDate(LocalDateTime.now());
+
+        DB.replace(memberDto.getEmail(), member);
+    }
+
+    @Override
+    public void updatePassword(String email, String password) {
+        Member member = DB.get(email);
+
+        member.setPassword(password);
+        member.setUpdatedDate(LocalDateTime.now());
+
+        DB.replace(email, member);
+    }
+
+    @Override
+    public void deleteMember(String email) {
+        DB.remove(email);
+    }
 }
