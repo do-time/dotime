9d289a36059af12e7d9b9f0ad29e3d65d301ca19
hoonbro <wlgns1588@naver.com>
2023-02-04 20:25:48 +0900

    feat: [20230204][변지훈] getMember 메서드 추가 - ApiResult를 사용한 getMember 메서드 추가

--- src/main/java/com/jhd/dotime/members/controller/MemberController.java
+++ src/main/java/com/jhd/dotime/members/controller/MemberController.java
@@ -1,7 +1,22 @@
 package com.jhd.dotime.members.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
+import com.jhd.dotime.members.entity.Member;
+import com.jhd.dotime.members.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static com.jhd.dotime.common.ApiUtils.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member/{email}")
+    public ApiResult<Member> getMember(@PathVariable String email){
+        return success(memberService.getMember(email));
+    }
 }
