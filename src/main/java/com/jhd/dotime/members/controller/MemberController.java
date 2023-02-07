e02dbb9d958237f6ae8327e18a1b771f9a69e84f
hoonbro <wlgns1588@naver.com>
2023-02-07 23:07:28 +0900

    update: [20230207][변지훈] createMember 메서드 추가 - createMember 메서드 추가 - CrossOrigin 어노테이션 추가

--- src/main/java/com/jhd/dotime/members/controller/MemberController.java
+++ src/main/java/com/jhd/dotime/members/controller/MemberController.java
@@ -1,22 +1,33 @@
 package com.jhd.dotime.members.controller;
 
 
+import com.jhd.dotime.members.common.exception.NotFoundException;
+import com.jhd.dotime.members.dto.MemberDto;
 import com.jhd.dotime.members.entity.Member;
 import com.jhd.dotime.members.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.jhd.dotime.common.ApiUtils.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/member")
+@CrossOrigin("*")
+@RequestMapping("/api/v1")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/member/{email}")
-    public ApiResult<Member> getMember(@PathVariable String email){
-        return success(memberService.getMember(email));
+    @PostMapping("/member")
+    public ResponseEntity<Void> createMember(@RequestBody MemberDto memberDto) {
+        memberService.createMember(memberDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Member> getMember(@PathVariable String email) {
+        return new ResponseEntity<Member>(memberService.getMember(email).orElseThrow(() -> new NotFoundException("Member does not exist")), HttpStatus.OK);
     }
 }
