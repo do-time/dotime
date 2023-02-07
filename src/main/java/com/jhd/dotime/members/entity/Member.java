b38bc6a58924e1d2afe569941cd455fdd158e0f4
hoonbro <wlgns1588@naver.com>
2023-02-07 23:06:10 +0900

    feat: [20230207][변지훈] Entity 설정 추가 - Builder 어노테이션 추가 - Auto Increment 추가 - CreateDate, LastModifiedDate 추가(해당 어노테이션 사용을 위해 MappedSupperclass 필요)

--- src/main/java/com/jhd/dotime/members/entity/Member.java
+++ src/main/java/com/jhd/dotime/members/entity/Member.java
@@ -1,18 +1,23 @@
 package com.jhd.dotime.members.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
+@Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String email;
@@ -23,7 +28,9 @@ public class Member {
 
     private String profileImage;
 
+    @CreatedDate
     private LocalDateTime createdDate;
 
+    @LastModifiedDate
     private LocalDateTime updatedDate;
 }
