c2c5a89b7937b3832195c3bf1fb3ac692cbd71a7
hoonbro <wlgns1588@naver.com>
2023-02-12 16:56:14 +0900

    delete: [20230212][변지훈] service 단위테스트 변경중 - save() 메서드 미동작중

--- src/main/java/com/jhd/dotime/members/repository/MemberRepository.java
+++ src/main/java/com/jhd/dotime/members/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.jhd.dotime.members.repository;
 
 import com.jhd.dotime.members.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String Email);
