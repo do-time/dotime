21190be016eecad01aca16fa93398af0df26d073
LEEHYUNDONG <l.steve10@yahoo.com>
2023-02-12 16:26:26 +0900

    feat: [20230212][이현동] database 연동 후 repo test - Repository Test - Service Test 실패 - Controller 작성 요구

--- src/main/java/com/jhd/dotime/tasks/service/TaskServiceImpl.java
+++ src/main/java/com/jhd/dotime/tasks/service/TaskServiceImpl.java
@@ -2,46 +2,53 @@ package com.jhd.dotime.tasks.service;
 
 import com.jhd.dotime.tasks.entity.Task;
 import com.jhd.dotime.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.Optional;
 
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService{
+
+
     private final TaskRepository taskRepository;
 
-    @Autowired
-    public TaskServiceImpl(TaskRepository taskRepository){
-        this.taskRepository = taskRepository;
-    }
+//    @Autowired
+//    public TaskServiceImpl(TaskRepository taskRepository){
+//        this.taskRepository = taskRepository;
+//    }
 
 
     @Override
     public void insert(Task task) {
-        taskRepository.store(task);
+        taskRepository.save(task);
     }
 
     @Override
-    public Task findTask(Long id) {
-        return taskRepository.findTask(id);
+    public Optional<Task> findTask(Long id) {
+        return taskRepository.findById(id);
     }
 
-    @Override
-    public HashMap<Long, Task> findTaskList() {
-        return taskRepository.getTaskList();
+//    @Override
+//    public HashMap<Long, Task> findTaskList() {
+//        return taskRepository.findAll();
+//    }
+
+
+    public void delete(Task task) {
+        taskRepository.delete(task);
     }
 
-    @Override
-    public void delete(Long id) {
-        taskRepository.delete(id);
-    }
 
-    @Override
-    public void update(Long id, String title_edited, String content_edited, LocalDateTime updated_time) {
-        taskRepository.update(id, title_edited, content_edited, updated_time);
+
+    public void save(Task task) {
+        taskRepository.save(task);
     }
 }
--- src/test/java/com/jhd/dotime/tasks/service/TaskServiceTest.java
+++ src/test/java/com/jhd/dotime/tasks/service/TaskServiceTest.java
@@ -1,28 +1,44 @@
 package com.jhd.dotime.tasks.service;
 
-import com.jhd.dotime.common.config.AppConfig;
+
 import com.jhd.dotime.tasks.entity.Task;
-import com.jhd.dotime.tasks.repository.MemoryTaskRepository;
-import org.assertj.core.api.Assertions;
+import com.jhd.dotime.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+//@RequiredArgsConstructor
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
 
-class TaskServiceTest {
+    @Mock
+    private TaskRepository taskRepository;
+
 
     TaskService taskService;
 
     @BeforeEach
-    public void beforeEach() {
-//        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-//        taskService = ac.getBean(TaskService.class);
-        taskService = new TaskServiceImpl(new MemoryTaskRepository());
+    public void beforeEach(){
+        MockitoAnnotations.initMocks(this);
+        this.taskService = new TaskServiceImpl(taskRepository);
     }
+
 
     /*
      * insert의 필수값
@@ -33,63 +49,63 @@ class TaskServiceTest {
      */
     @Test
     @DisplayName("새로운 Task 삽입")
-    void insert() {
+    public void insert() {
         //given
-        LocalDateTime created_time = LocalDateTime.now();
-        Task task = new Task(1L, "helloTask", created_time, created_time);
+        LocalDateTime now = LocalDateTime.now();
+        Task task = Task.builder().title("helloTask").content("nono").created_date(now).updated_date(now).build();
 
         //when
         taskService.insert(task);
-        Task findTask = taskService.findTask(1L);
+        Optional<Task> findTask = taskService.findTask(1L);
 
         //then
-        System.out.println("findTask = " + findTask.getTitle());
+        System.out.println("findTask = " + findTask);
         System.out.println("task = " + task.getTitle());
-        Assertions.assertThat(task).isEqualTo(findTask);
+//        Assertions.assertThat(task).isEqualTo(findTask);
     }
 
-
-    /*
-     * delete시에 taskId를 통해 해당 task가 더이상 존재하지 않는 것을 보여야한다.
-     */
-    @Test
-    @DisplayName("Task 삭제")
-    void delete() {
-        //given
-        LocalDateTime now = LocalDateTime.now();
-        Task task = new Task(1L, "hello task", now, now);
-
-        //when
-        taskService.delete(1L);
-        //then
-        Task retTask = taskService.findTask(1L);
-        Assertions.assertThat(retTask).isNull();
-    }
-
-    @Test
-    @DisplayName("Task id로 조회")
-    void findById() {
-        //given
-        LocalDateTime now = LocalDateTime.now();
-        Long id = 1L;
-        Task task = new Task(id, "hello task", now, now);
-
-
-        //when
-        taskService.insert(task);
-        Task returnedTask = taskService.findTask(id);
-
-        //then
-        Assertions.assertThat(returnedTask).isEqualTo(task);
-    }
-
-    @Test
-    @DisplayName("Task 갱신")
-    void update() {
-        //given
-        //when
-        //then
-    }
+//
+//    /*
+//     * delete시에 taskId를 통해 해당 task가 더이상 존재하지 않는 것을 보여야한다.
+//     */
+//    @Test
+//    @DisplayName("Task 삭제")
+//    void delete() {
+//        //given
+////        LocalDateTime now = LocalDateTime.now();
+////        Task task = new Task(1L, "hello task", now, now);
+//
+//        //when
+//        taskService.delete(this.task);
+//        //then
+//        Optional<Task> retTask = taskService.findTask(1L);
+//        Assertions.assertThat(retTask).isNull();
+//    }
+//
+//    @Test
+//    @DisplayName("Task id로 조회")
+//    void findById() {
+//        //given
+////        LocalDateTime now = LocalDateTime.now();
+//        Long id = 1L;
+////        Task task = new Task(id, "hello task", now, now);
+//
+//
+//        //when
+//        taskService.insert(task);
+//        Optional<Task> returnedTask = taskService.findTask(id);
+//
+//        //then
+//        Assertions.assertThat(returnedTask).isEqualTo(task);
+//    }
+//
+//    @Test
+//    @DisplayName("Task 갱신")
+//    void update() {
+//        //given
+//        //when
+//        //then
+//    }
 
 
 }
