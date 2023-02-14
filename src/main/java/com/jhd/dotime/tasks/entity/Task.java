3acb5e04903094ba7750e50dbe83a0dc5a372a65
LEEHYUNDONG <hd.lee1@cj.net>
2023-02-14 14:47:43 +0900

    feat :[20230214][이현동] Task Entity, Base Time으로 확장 - Member 수정 완료하면 관계 매핑해야함

--- src/main/java/com/jhd/dotime/tasks/entity/Task.java
+++ src/main/java/com/jhd/dotime/tasks/entity/Task.java
@@ -1,18 +1,15 @@
 package com.jhd.dotime.tasks.entity;
 
+import com.jhd.dotime.common.entity.BaseTimeEntity;
 import lombok.*;
-
-import javax.annotation.Nullable;
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-public class Task {
+@Entity(name="task")
+public class Task extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,30 +17,17 @@ public class Task {
     private String title;
     @Column
     private String content;
-    @Column
-    private LocalDateTime create_date;
-    @Column
-    private LocalDateTime updated_date;
-
-//    @Column
-//    private Long member_id;
-
-
-//    public Task(Task task){
-//        this.id = task.getId();
-//        this.title = task.getTitle();
-//        this.content = task.getContent();
-//        this.create_date = task.getCreate_date();
-//        this.updated_date = task.getUpdated_date();
-////        this.member_id = task.getMember_id();
-//    }
 
     @Builder
-    public Task(String title, String content, LocalDateTime created_date, LocalDateTime updated_date) {
+    public Task(String title, String content){
         this.title = title;
         this.content = content;
-        this.create_date = created_date;
-        this.updated_date = updated_date;
+    }
+
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 
 
