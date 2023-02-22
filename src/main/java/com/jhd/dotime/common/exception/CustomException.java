b28e5520c87fa5428a2fcef2cf62e242fd687aee
LEEHYUNDONG <hd.lee1@cj.net>
2023-02-22 11:13:07 +0900

    feat :[20230222][이현동] Common Exception 작성 및 Task Service 적용 - ErrorCode를 작성하여 ErrorResponse로 Build - GlobalExceptionHandler로 처리

--- src/main/java/com/jhd/dotime/common/exception/CustomException.java
+++ src/main/java/com/jhd/dotime/common/exception/CustomException.java
@@ -1,6 +1,7 @@
 package com.jhd.dotime.common.exception;
 
 
+import com.jhd.dotime.common.error.ErrorCode;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -8,19 +9,29 @@ import org.springframework.http.HttpStatus;
 public class CustomException extends RuntimeException{
 
 
-    private final String message;
-    private final HttpStatus status;
+//    private final String message;
+//    private final HttpStatus status;
+    private final ErrorCode errorCode;
 
-
-    public CustomException(String message, HttpStatus status, Throwable reason) {
-        super(message, reason);
-        this.status = status;
-        this.message = message;
+    public CustomException(ErrorCode errorCode, Throwable reason){
+        super(errorCode.getMessage(), reason);
+        this.errorCode = errorCode;
     }
 
-    public CustomException(String message, HttpStatus status) {
-        super(message);
-        this.status = status;
-        this.message = message;
+    public CustomException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
+
+//    public CustomException(String message, HttpStatus status, Throwable reason) {
+//        super(message, reason);
+//        this.status = status;
+//        this.message = message;
+//    }
+//
+//    public CustomException(String message, HttpStatus status) {
+//        super(message);
+//        this.status = status;
+//        this.message = message;
+//    }
 }
