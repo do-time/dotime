cd66746523dd9509c061beaa71a427eb124e8abd
= <wlgns1588@naver.com>
2023-02-22 18:06:02 +0900

    update: [20230222][변지훈] Repository Test 수정중 - update, delete 테스트 수정중

--- src/test/java/com/jhd/dotime/members/repository/MemberRepositoryTest.java
+++ src/test/java/com/jhd/dotime/members/repository/MemberRepositoryTest.java
@@ -4,12 +4,12 @@ import com.jhd.dotime.members.common.exception.NotFoundException;
 import com.jhd.dotime.members.entity.Member;
 import com.jhd.dotime.tasks.entity.Task;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
@@ -17,27 +17,33 @@ import java.util.ArrayList;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@MockBean(JpaMetamodelMappingContext.class)
 public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
+
+    @BeforeAll
+    public static void beforeAll(){
+        member = Member.builder()
+                .email("test@test.com")
+                .password("1234")
+                .username("testMan")
+                .profileImage("")
+                .build();
+    }
 
     @AfterEach
     public void afterEach(){
         memberRepository.deleteAllInBatch();
     }
 
+    static Member member;
+
     @Test
     @DisplayName("멤버 저장")
     public void createMember() {
         // given
-        Member member = Member.builder()
-                .email("test@test.com")
-                .password("1234")
-                .username("testMan")
-                .profileImage("")
-//                .createdDate(LocalDateTime.now())
-//                .updatedDate(LocalDateTime.now())
-                .build();
+        long count = memberRepository.count();
         // when
         Member savedMember = memberRepository.save(member);
 
@@ -45,29 +51,68 @@ public class MemberRepositoryTest {
         Assertions.assertThat(member).isSameAs(savedMember);
         Assertions.assertThat(savedMember.getId()).isNotNull();
         Assertions.assertThat(member.getEmail()).isEqualTo(savedMember.getEmail());
-        Assertions.assertThat(memberRepository.count()).isEqualTo(1);
-        System.out.println(savedMember.toString());
+        Assertions.assertThat(memberRepository.count()).isEqualTo(count+1);
     }
 
     @Test
     @DisplayName("멤버 조회")
     public void findMember() {
         // given
-        Member newMember = Member.builder()
+        Member member = Member.builder()
                 .email("test@test.com")
                 .password("1234")
                 .username("testMan")
                 .profileImage("")
                 .build();
-        memberRepository.save(newMember);
+
+        memberRepository.save(member);
         // when
         Member findMember = memberRepository.findByEmail("test@test.com").orElseThrow(() -> new NotFoundException("Member does not exist"));
 
         // then
-        Assertions.assertThat(findMember.getEmail().equals(newMember.getEmail()));
-        Assertions.assertThat(findMember.getPassword().equals(newMember.getPassword()));
-        Assertions.assertThat(findMember.getUsername().equals(newMember.getUsername()));
+        Assertions.assertThat(findMember.getEmail().equals(member.getEmail()));
+        Assertions.assertThat(findMember.getPassword().equals(member.getPassword()));
+        Assertions.assertThat(findMember.getUsername().equals(member.getUsername()));
+        //Assertions.assertThat(memberRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("멤버 정보 변경")
+    public void updateMember() {
+        // given
+        Member member = Member.builder()
+                .email("test@test.com")
+                .password("1234")
+                .username("testMan")
+                .profileImage("")
+                .build();
+        memberRepository.save(member);
+
+        member.updateInfo("update name");
+
+        // when
+        Member findMember = memberRepository.findByEmail("test@test.com").orElseThrow(() -> new NotFoundException("Member does not exist"));
+
+        // then
+        Assertions.assertThat(findMember.getUsername().equals(member.getUsername()));
         Assertions.assertThat(memberRepository.count()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("멤버 삭제")
+    public void deleteMember() {
+        // given
+        long count = memberRepository.count();
+
+        memberRepository.save(member);
+
+        // when
+        memberRepository.delete(member);
+
+        // then
+        Assertions.assertThat(memberRepository.count()).isEqualTo(count-1);
+        Assertions.assertThatThrownBy(() -> {
+            memberRepository.findByEmail("test@test.com").orElseThrow(() -> new NotFoundException("Member does not exist"));
+        }).withFailMessage("Member does not exist");
+    }
 }
