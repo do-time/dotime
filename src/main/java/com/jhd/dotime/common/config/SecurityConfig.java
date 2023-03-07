c4b6a6297be761305614e9a015b096e172e4f9eb
hoonbro <wlgns1588@naver.com>
2023-03-07 22:16:55 +0900

    feat: [20230307][변지훈] SpringSecurity Config 추가 - 이전 버전 Spring Security 기본 설정 추가 - Role 미추가로 JWT개발 시 MEMBER entity에 Role 추가해 개발 진행 예정

--- src/main/java/com/jhd/dotime/common/config/SecurityConfig.java
+++ src/main/java/com/jhd/dotime/common/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jhd.dotime.common.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -9,6 +10,22 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http
+                .authorizeRequests()// 보호된 리소스 URI에 접근할 수 있는 권한을 설정
+                .antMatchers("/api/v1/member/**").permitAll()
+                .antMatchers("/api/v1/task/**").permitAll()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        ;
+    }
+
+    /*
+     * 스프링 시큐리티 룰을 무시하게 하는 Url 규칙(여기 등록하면 규칙 적용하지 않음)
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**")
+        ;
     }
 }
