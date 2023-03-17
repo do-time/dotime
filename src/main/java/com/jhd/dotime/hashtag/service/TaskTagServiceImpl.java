98b62e876ad56548ae742f723d1afb5bc0bd0ab5
LEEHYUNDONG <l.steve10@yahoo.com>
2023-03-17 12:46:46 +0900

    fix :[20230317][이현동] task 생성 -> hashtag, tasktag 생성 - 현재 중복된 hashtag가 생성되어 있음. - 중복된 hashtag 생성을 방지해야한다.

--- src/main/java/com/jhd/dotime/hashtag/service/TaskTagServiceImpl.java
+++ src/main/java/com/jhd/dotime/hashtag/service/TaskTagServiceImpl.java
@@ -2,8 +2,13 @@ package com.jhd.dotime.hashtag.service;
 
 import com.jhd.dotime.hashtag.entity.HashTag;
 import com.jhd.dotime.hashtag.entity.TaskTag;
+import com.jhd.dotime.hashtag.repository.HashTagRepository;
 import com.jhd.dotime.hashtag.repository.TaskTagRepository;
-import lombok.NoArgsConstructor;
+import com.jhd.dotime.members.common.exception.NotFoundException;
+import com.jhd.dotime.tasks.common.error.TaskErrorCode;
+import com.jhd.dotime.tasks.common.exception.TaskException;
+import com.jhd.dotime.tasks.entity.Task;
+import com.jhd.dotime.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +21,21 @@ import java.util.List;
 public class TaskTagServiceImpl implements TaskTagService{
     private final TaskTagRepository taskTagRepository;
 
+    private final TaskRepository taskRepository;
+
+    private final HashTagRepository hashTagRepository;
+
     @Override
-    public Long createTaskTag(Long memberId, Long taskId) {
-        return null;
+    public Long createTaskTag(Long taskId, List<Long> hashtagIdLst) {
+
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new TaskException(TaskErrorCode.TASK_NOT_FOUNT));;
+        for (Long hashtagId : hashtagIdLst) {
+            HashTag hashTag = hashTagRepository.findById(hashtagId).orElseThrow(() -> new NotFoundException("해시태그가 존재하지 않습니다."));
+
+            taskTagRepository.save(TaskTag.builder().task(task).hashTag(hashTag).build());
+        }
+
+        return taskId;
     }
 
     @Override
