bd9608be80b9df8c76b16088994cbe2795e96736
hoonbro <wlgns1588@naver.com>
2023-03-23 23:14:49 +0900

    update: [20230313][변지훈] JWT 오류 해결 - 회원가입시 권한 입력되게 변경 - LoginRequestDto 컬럼 email로 변경(username으로 되어있어 오류 발생)

--- src/main/java/com/jhd/dotime/members/mapper/MemberMapper.java
+++ src/main/java/com/jhd/dotime/members/mapper/MemberMapper.java
@@ -1,10 +1,16 @@
 package com.jhd.dotime.members.mapper;
 
+import com.jhd.dotime.auth.entity.Authority;
 import com.jhd.dotime.members.dto.MemberRequestDto;
 import com.jhd.dotime.members.dto.MemberResponseDto;
 import com.jhd.dotime.members.entity.Member;
 import org.mapstruct.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
 
 @Mapper(
         componentModel = "spring",
@@ -17,6 +23,7 @@ public interface MemberMapper {
             source = "password", target = "password",
             qualifiedByName = "encryptPassword"
     )
+    @Mapping(target = "authorities", expression = "java(mapAuthority())")
     Member toEntity(MemberRequestDto memberRequestDto);
 
     @Mapping(target = "taskList", ignore = true)
@@ -25,5 +32,12 @@ public interface MemberMapper {
     @Named("encryptPassword")
     default String encryptPassword(String password){
         return new BCryptPasswordEncoder().encode(password);
+    }
+
+    default Set<Authority> mapAuthority(){
+        Authority authority = Authority.builder()
+                .authorityName("USER_ROLE")
+                .build();
+        return Collections.singleton(authority);
     }
 }
