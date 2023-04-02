6000ae6657fdede80a1fcb14218fe442b7bc8a4d
LEEHYUNDONG <l.steve10@yahoo.com>
2023-04-02 14:46:34 +0900

    [230402][이현동] Task dto

---
+++ src/main/java/com/jhd/dotime/tasks/dto/TaskDto.java
@@ -0,0 +1,59 @@
+package com.jhd.dotime.tasks.dto;
+
+
+import com.jhd.dotime.hashtag.dto.HashTagDto;
+import com.jhd.dotime.tasks.entity.Task;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TaskDto {
+
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Request {
+
+        @NonNull
+        private String title;
+
+        private String content;
+        private String hashtag;
+
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+
+        @NonNull
+        private Long id;
+        private String title;
+        private String content;
+        private List<HashTagDto.Response> hashtag;
+        private LocalDateTime createdDate;
+        private LocalDateTime updatedDate;
+
+        public static Response of(Task task) {
+            return Response.builder()
+                    .id(task.getId())
+                    .title(task.getTitle())
+                    .content(task.getContent())
+                    .createdDate(task.getCreatedDate())
+                    .updatedDate(task.getUpdatedDate())
+                    .hashtag(task.getHashTag().stream().map(HashTagDto.Response::of).collect(Collectors.toList()))
+                    .build();
+
+        }
+
+    }
+
+
+
+}
