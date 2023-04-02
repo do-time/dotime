f35b74680858f87c8a6201d10772b8b1dca0e527
hoonbro <wlgns1588@naver.com>
2023-04-02 16:33:07 +0900

    update: [20230402][변지훈] HashTag equals, hashcode 재정의 - equals, hashcode 재정의를 통해 객체 같은 값으로 보이게 변경

--- src/main/java/com/jhd/dotime/hashtag/entity/HashTag.java
+++ src/main/java/com/jhd/dotime/hashtag/entity/HashTag.java
@@ -2,9 +2,11 @@ package com.jhd.dotime.hashtag.entity;
 
 
 import com.jhd.dotime.common.entity.BaseTimeEntity;
+import com.jhd.dotime.members.entity.Member;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 
 @Getter
@@ -22,5 +24,22 @@ public class HashTag extends BaseTimeEntity {
     @Column
     private String name;
 
+    @Override
+    public boolean equals(Object o){
+        if(this == o)
+            return true;
 
+        if(!(o instanceof HashTag))
+            return false;
+
+        HashTag hashTag = (HashTag) o;
+
+        return hashTag.id == id &&
+                hashTag.name.equals(name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id, name);
+    }
 }
