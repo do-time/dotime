d4b54a75f6120c7d78e1f0a2deec58ffbd6b33a5
LEEHYUNDONG <l.steve10@yahoo.com>
2023-04-11 16:01:05 +0900

    feat: [2023041107][이현동] chat initial commit

--- src/main/java/com/jhd/dotime/chat/entity/ChatRoom.java
+++ src/main/java/com/jhd/dotime/chat/entity/ChatRoom.java
@@ -1,4 +1,36 @@
 package com.jhd.dotime.chat.entity;
 
-public class ChatRoom {
+import com.jhd.dotime.common.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name="chatroom")
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @Column(name="chatroom_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name="people_count")
+    private Long people_count;
+
+    @Builder
+    public ChatRoom(String title, Long people_count){
+        this.title = title;
+        this.people_count = people_count;
+    }
+
+
 }
