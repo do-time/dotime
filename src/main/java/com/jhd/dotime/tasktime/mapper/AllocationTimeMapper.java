01e4fa9740ca1dcfb2778682e3641e210f5a1a1e
hoonbro <wlgns1588@naver.com>
2023-04-21 00:10:11 +0900

    feat: [20230420][변지훈] Allocation Time init  - Mapper, Exception, ErrorCode init

---
+++ src/main/java/com/jhd/dotime/tasktime/mapper/AllocationTimeMapper.java
@@ -0,0 +1,34 @@
+package com.jhd.dotime.tasktime.mapper;
+
+import com.jhd.dotime.tasks.entity.Task;
+import com.jhd.dotime.tasktime.dto.AllocationTimeDto;
+import com.jhd.dotime.tasktime.dto.TaskTimeLogDto;
+import com.jhd.dotime.tasktime.entity.AllocationTime;
+import com.jhd.dotime.tasktime.entity.TaskTimeLog;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR, // 생성자 주입 전략
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface AllocationTimeMapper {
+    default AllocationTime toEntity(AllocationTimeDto.Request requestDto, Task task){
+        return AllocationTime.builder()
+                .type(requestDto.getType())
+                .time(requestDto.getTime())
+                .task(task)
+                .build();
+    }
+
+    default TaskTimeLogDto.Response toResponseDto(TaskTimeLog taskTimelog){
+        return TaskTimeLogDto.Response.builder()
+                .id(taskTimelog.getId())
+                .taskId(taskTimelog.getTask().getId())
+                .startTime(taskTimelog.getStartTime())
+                .endTime(taskTimelog.getEndTime())
+                .build();
+    }
+}
