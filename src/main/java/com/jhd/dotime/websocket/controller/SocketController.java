0f4bac0a4d3465ce7e6a9708daccfef788f45258
LEEHYUNDONG <l.steve10@yahoo.com>
2023-05-07 17:46:40 +0900

    feat :[230507][이현동] Chat Room 입장 수정

--- src/main/java/com/jhd/dotime/websocket/controller/SocketController.java
+++ src/main/java/com/jhd/dotime/websocket/controller/SocketController.java
@@ -1,11 +1,14 @@
 package com.jhd.dotime.websocket.controller;
 
 
+import com.jhd.dotime.chat.dto.ChatMessageDto;
 import com.jhd.dotime.chat.entity.ChatMessage;
+import com.jhd.dotime.members.entity.Member;
 import com.jhd.dotime.websocket.service.SocketService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -17,11 +20,12 @@ public class SocketController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping(value="/chat/enter")
-    public void enterChatRoom(ChatMessage chatMessage){
+    public void enterChatRoom(ChatMessageDto.Request chatRequest){
         System.out.println("연결성공");
-        chatMessage.setContent(chatMessage.getSender() + "님이 채팅방에 참여하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getChatRoom().getId(), chatMessage);
+        ChatMessage chatMessage = ChatMessage.builder().content("지훈스" + "님이 채팅방에 참여하셨습니다.").sender(null).build();
+        messagingTemplate.convertAndSend("/sub/chat/room/" + chatRequest.getRoomId(), chatMessage);
     }
+
 
     @MessageMapping(value = "/chat/message")
     public void sendMessage(ChatMessage chatMessage) {
