9ac77f438f30a1015d88577d3f536350ca339a13
= <wlgns1588@naver.com>
2023-08-11 17:43:08 +0900

    update: [20230811][변지훈] MemberTest 재작성중 - createMember - 버전 꼬이면서 날아감..

--- src/test/java/com/jhd/dotime/members/service/MemberServiceTest.java
+++ src/test/java/com/jhd/dotime/members/service/MemberServiceTest.java
@@ -14,20 +14,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
 
-//    @InjectMocks
+    @InjectMocks
     private MemberServiceImpl memberService;
 
     @Mock
-
     private MemberRepository memberRepository;
 
     @InjectMocks
@@ -36,48 +39,35 @@ class MemberServiceTest {
     @Mock
     private TaskRepository taskRepository;
 
-
-
-    /*
-     *
-     * create 테스트에서 find를 쓰는게 맞을까?
-     * UI/UX에서 회원가입한 회원에게 회원정보를 보여주거나 하는 과정이 있으면 member return해도 될듯
-     *
-     */
     @Test
     @DisplayName("Member 회원 가입 성공 테스트")
     public void createMember() {
         //given
-        LocalDateTime now = LocalDateTime.now();
         MemberDto.Request newMember = new MemberDto.Request("test@test.com", "testMan", "1234", "");
+
         Member member = Member.builder()
-                .email("11111@test.com")
+                .email("test@test.com")
                 .password("1234")
                 .username("testMan")
                 .profileImage("")
-//                .createdDate(now)
-//                .updatedDate(now)
                 .build();
 
-//        Long fakeMemberId = 1l;
-//        ReflectionTestUtils.setField(member, "id", fakeMemberId);
+        Long fakeMemberId = 1l;
+        ReflectionTestUtils.setField(member, "id", fakeMemberId);
 
-//        System.out.println(newMember.getEmail().equals(newmember.getEmail()));
-//        System.out.println(member.toString());
-
-//        given(memberRepository.save(any())).willReturn(member);
+        given(memberRepository.save(any(Member.class))).willReturn(member);
+        given(memberRepository.findByEmail(member.getEmail())).willReturn(Optional.of(member));
 
         //when
         memberService.createMember(newMember);
-//        Member findMember = memberRepository.findByEmail(member.getEmail()).orElseThrow(() -> new NotFoundException("Member does not exist"));
         Member findMember = memberRepository.findByEmail(member.getEmail()).orElseThrow(() -> new NotFoundException("Member does not exist"));
-//        System.out.println(findMember.toString());
 
-
-//        Member findMember = memberRepository.findById(1l).orElseThrow();
         //then
-        Assertions.assertThat(findMember).isNotNull();
-//        Assertions.assertThat(findMember.getEmail()).isEqualTo(newMember.getEmail());
+        Assertions.assertThat(findMember.getEmail()).isEqualTo(newMember.getEmail());
+        assertAll(
+                () -> findMember.getEmail().equals(newMember.getEmail()),
+                () -> findMember.getEmail().equals(newMember.getEmail())
+        );
     }
 
     @Test
